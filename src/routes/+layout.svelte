cf4cd3b79ee5861b1b5f881386b9545669b5c54b
RashidBik <abdurashid.bigzad1999@gmial.com>
2025-11-06 11:03:10 +0430

    logo added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,23 +7,12 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-<header style="display:flex; align-items:center; justify-content:end; padding:1rem; background:#f8f8f8;">
+<header style="display:flex; align-items:center; justify-content:end; padding:1rem; background:linear-gradient(120deg, #3182ce, #74b9ff);">
 	<img style="height: 40px; margin-right: 1rem;" src="images/favicon.png" alt="logo" srcset="">
 	<h1 class="logo">Space4</h1>
 </header>
 <style>
-	@media (max-width: 768){
-		.logo{
-			width: 100px;
-		}
-	}
-
-	@media (max-width: 480){
-		.logo{
-			width: 70px;
-		}
-	}
-	
+	@media (max-width: 768px){.logo{display: none;}}	
 </style>
 
 {@render children()}
